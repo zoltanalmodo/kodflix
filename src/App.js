7ec684c64a2c7f1746efa2f74c092afa623841da
zoltan_almodo <zoltanalmodo@gmail.com>
2018-07-02 11:13:10 +0100

    first_issue_final

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import cover_001 from './cover_001.jpg';
-import cover_002 from './cover_002.jpg';
-import cover_003 from './cover_003.jpg';
 
 
 class App extends Component {
@@ -11,8 +9,6 @@ class App extends Component {
       <div className="App">
 
           <img src={cover_001} className="image-cover" alt="cover_001"/>
-          <img src={cover_002} className="image-cover" alt="cover_002"/>
-          <img src={cover_003} className="image-cover" alt="cover_003"/>
 
       </div>
     );
