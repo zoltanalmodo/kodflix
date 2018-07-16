2846b589b7cfa8fad9d33189f0e71ff59472b8a5
zoltan_almodo <zoltanalmodo@gmail.com>
2018-07-16 19:31:42 +0100

    issue_15_back_link_added

--- src/not_found/NotFound.js
+++ src/not_found/NotFound.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function NotFound() {
     return (
         <div>
             <h1>"Ooops, it looks like this page doesn't exist :("</h1>
-        </div>    
+            <Link to={'/'}>
+                <h1 className='home_button'>
+                    back
+                </h1>
+            </Link>
+        </div>
     )
 }
 
-        
-    
+
+
 
