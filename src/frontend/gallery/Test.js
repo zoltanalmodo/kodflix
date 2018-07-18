51613803f1c0e99cfa80fba6cbd31d27da4ed1d6
zoltan_almodo <zoltanalmodo@gmail.com>
2018-07-18 15:48:08 +0100

    18_first_commit_working

--- src/frontend/gallery/Test.js
+++ src/frontend/gallery/Test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Gallery.css';
-import Hello from './Hello';
 
 export default class Cool extends React.Component {
 
--- src/frontend/not_found/NotFound.js
+++ src/frontend/not_found/NotFound.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
     return (
         <div>
-            <h1>"Ooops, it looks like this page doesn't exist :("</h1>
+            <h1>"Ooops, this page doesn't exist :("</h1>
             <Link to={'/'}>
                 <h1 className='home_button'>
                     back
