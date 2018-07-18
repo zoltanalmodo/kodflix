6a3597a7330cbf08c1af0ebcfa853cd0ffe3eaca
zoltan_almodo <zoltanalmodo@gmail.com>
2018-07-18 16:06:18 +0100

    19_final_working

--- src/frontend/gallery/getGallery.js
+++ src/frontend/gallery/getGallery.js
@@ -1,10 +1,16 @@
 
-import dinosaur_walkman_eye from '../cover/images/dinosaur_walkman_eye.png';
-import blue_ufo_plane from '../cover/images/blue_ufo_plane.png';
-import elephant_king_jelly from '../cover/images/elephant_king_jelly.png';
-import green_alien_spaceship from '../cover/images/green_alien_spaceship.png';
-import outer_space_jelly from '../cover/images/outer_space_jelly.png'
-import pink_elephant_plane from '../cover/images/pink_elephant_plane.png';
+import dinosaur_walkman_eye from
+    '../cover/images/dinosaur_walkman_eye.png';
+import blue_ufo_plane from
+    '../cover/images/blue_ufo_plane.png';
+import elephant_king_jelly from
+    '../cover/images/elephant_king_jelly.png';
+import green_alien_spaceship from
+    '../cover/images/green_alien_spaceship.png';
+import outer_space_jelly from
+    '../cover/images/outer_space_jelly.png'
+import pink_elephant_plane from
+    '../cover/images/pink_elephant_plane.png';
 
 export default function getGallery() {
     return [
