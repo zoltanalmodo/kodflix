e0f6cec8ff11d4c522ed6c716c88892e8633178e
zoltan_almodo <zoltanalmodo@gmail.com>
2018-07-24 18:28:31 +0100

    issue_23_connect the frontend and backend servers

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 const getShows = require('./shows');
+const path = require('path');
+const port = process.env.PORT || 3001;
 
-app.get('/rest/shows', (req, res) => res.send(getShows()));
+app.get ('/rest/shows', (req, res) => res.send(getShows()));
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'));
+app.use (express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
