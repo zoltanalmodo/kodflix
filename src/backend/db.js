cb7cfe3bfb9e7acc29825ad6dcc46968a2ec4653
zoltan_almodo <zoltanalmodo@gmail.com>
2018-08-23 22:05:24 +0100

    created db.js

---
+++ src/backend/db.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://kodflix:kodflix@127.0.0.1:27017/kodflix";
+
+
+function connect() {
+  return new Promise((resolve, reject) => {
+      MongoClient.connect(url, function(err, db) {
+    if (err) reject (err);
+    var dbo = db.db("kodiflix");
+      resolve(dbo);
+    });
+  });
+}
+module.exports = connect; ]
